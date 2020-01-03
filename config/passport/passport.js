12135b1078c1d6efa95230d49433578843f86bf9
ChayutMill <chayut.rod@gmail.com>
2020-01-03 14:26:28 +0700

    login

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -5,10 +5,11 @@ const passport = require('passport')
 const db = require('../../models')
 const localStrategy = require('passport-local').Strategy
 const JWTStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
+const ExtractJWT = require('passport-jwt').ExtractJwt
 const BCRYPT_SALT_ROUNDS = 12
 let jwtOption = {}
 jwtOption.secretOrKey = 'admin'
+
 passport.use('register', new localStrategy(
     {
         usernameField: 'email',
@@ -17,7 +18,7 @@ passport.use('register', new localStrategy(
     },
     (email, password, done) => {
         db.user.findOne({
-            where: { email: email }
+            where: { email }
         }).then(user => {
             if (user !== null) {
                 console.log('Email already taken')
@@ -38,3 +39,67 @@ passport.use('register', new localStrategy(
         })
     }
 ))
+passport.use('login', new localStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false,
+    },
+    (email, password, done) => {
+        try {
+            db.user.findOne({
+                where: { email },
+            }).then(user => {
+                if (user === null) {
+                    return done(null, false, { message: 'The email or password you entered is incorrect, please try again.' });
+                }
+                console.log(user.password)
+                bcrypt.compare(password, user.password, function (err, response) {
+                    console.log({ response })
+                    if (err) {
+                        console.log(err)
+                        done(err)
+                    }
+                    if (response !== true) {
+                        console.log('passwords do not match');
+                        return done(null, false, { message: 'passwords do not match' });
+                    }
+                    console.log('user found & authenticated');
+                    return done(null, user);
+                });
+            });
+        } catch (err) {
+            done(err);
+        }
+    },
+),
+);
+const opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwtOption.secretOrKey,
+};
+passport.use(
+    'jwt',
+    new JWTStrategy(opts, (jwt_payload, done) => {
+        try {
+            console.log("test")
+            db.user.findOne({
+                where: {
+                    id: jwt_payload.id,
+                },
+            }).then(user => {
+                if (user) {
+                    console.log('user found in db in passport');
+                    done(null, user);
+                } else {
+                    console.log('user not found in db');
+                    done(null, false);
+                }
+            });
+        } catch (err) {
+            console.log(err)
+            done(err);
+        }
+    }),
+);
+module.exports = { jwtOption, BCRYPT_SALT_ROUNDS }
--- service/user.js
+++ service/user.js
@@ -1,4 +1,9 @@
 const passport = require('passport');
+
+const jwt = require('jsonwebtoken');
+const config = require('../config/passport/passport');
+const bcrypt = require('bcryptjs')
+
 module.exports = (app, db) => {
     app.post('/registerUser', (req, res, next) => {
         passport.authenticate('register', (err, user, info) => {
@@ -32,4 +37,40 @@ module.exports = (app, db) => {
             }
         })(req, res, next);
     });
+
+    app.post('/loginUser', (req, res, next) => {
+        passport.authenticate('login', (err, user, info) => {
+          if (err) {
+            console.error(`error ${err}`);
+          }
+          if (info !== undefined) {
+            console.error(info.message);
+            if (info.message === 'The email or password you entered is incorrect, please try again.') {
+              res.status(401).send(info.message);
+            } else {
+              res.status(403).send(info.message);
+            }
+          } else {
+            db.user.findOne({
+              where: {
+                email: req.body.email,
+              },
+            }).then(user => {
+              const token = jwt.sign({
+                id: user.id,
+                role: user.role,
+                name: user.firstname,
+              }, config.jwtOption.secretOrKey, {
+                expiresIn: 3600,
+              });
+              res.status(200).send({
+                auth: true,
+                token,
+                message: 'user found & logged in',
+              });
+            });
+          }
+        })(req, res, next);
+      }
+    );
 }
